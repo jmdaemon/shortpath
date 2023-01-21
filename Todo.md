565aae7927891b3936482804505812bc5305fd9d
Joseph Diza <josephm.diza@gmail.com>
2023-01-20 18:06:02 -0800

    Update README

--- Todo.md
+++
@@ -1,9 +0,0 @@
-# Todo
-
-- Fix bug with fold_shortpath
-- Extract all subcommand functionality into `command` module.
-- Split up functions into more functions
-- Refactor exports with traits
-- Use more functional style
-    - Pass more higher order functions and closures around
-- Documentation
--- src/consts.rs
+++ src/consts.rs
@@ -2,7 +2,7 @@
 pub const PROGRAM_NAME: &str        = "shortpaths";
 pub const VERSION: &str             = "0.1.0";
 pub const AUTHOR: &str              = "Joseph Diza. <josephm.diza@gmail.com>";
-pub const PROGRAM_DESCRIPTION: &str = "Resilient directory path aliases";
+pub const PROGRAM_DESCRIPTION: &str = "Resilient file path aliases";
 
 pub const QUALIFIER: &str           = "com";
 pub const ORGANIZATION: &str        = "jmdaemon";
