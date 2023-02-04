2f9227f85f6130eedc675d6457d0c1c909d8228d
Joseph Diza <josephm.diza@gmail.com>
2023-02-03 22:12:25 -0800

    Move binary code from sp to unit test in sp

--- src/sp.rs
+++ src/sp.rs
@@ -447,3 +447,49 @@ pub fn update_shortpath(shortpaths: &mut SP, current_name: &str, name: Option<&S
         shortpaths.insert(new_name.to_owned(), path);
     } 
 }
+
+#[test]
+fn test_shortpaths() {
+    use crate::sp::{
+        SPT,
+        Shortpath,
+        ShortpathsBuilder,
+        FindKeyIndexMapExt,
+        sort_shortpaths,
+        export_shortpaths,
+    };
+
+    use std::path::PathBuf;
+
+    // TODO Create more ergonomic api for this later
+    // Wrap it together with the builder construct to reduce the noise
+    let sp_paths = vec![
+        Shortpath::new(SPT::new_path("d", PathBuf::from("$a/dddd")), None, None),
+        Shortpath::new(SPT::new_path("c", PathBuf::from("$b/cccc")), None, None),
+        Shortpath::new(SPT::new_path("b", PathBuf::from("$a/bbbb")), None, None),
+        Shortpath::new(SPT::new_path("a", PathBuf::from("aaaa")), None, None),
+    ];
+    println!("{:?}", sp_paths);
+
+    let mut sp_builder = ShortpathsBuilder::new(sp_paths);
+
+    let sp_im = sp_builder.build().unwrap();
+    sp_im.iter().for_each(|p| println!("{:?}", p));
+
+    // Test find_key
+    let key = sp_im.find_key_for_value("$a/bbbb");
+    println!("{:?}", key);
+
+    let key = sp_im.find_key_for_value("$a/bbbb".to_string());
+    println!("{:?}", key);
+
+    // Test sort_shortpaths
+    println!("Sorted list of shortpaths");
+    let sorted = sort_shortpaths(sp_im);
+    sorted.iter().for_each(|p| println!("{:?}", p));
+
+    // Test serialization
+    let export_type = "bash";
+    let output_file = None;
+    export_shortpaths(&sorted, export_type, output_file);
+}
