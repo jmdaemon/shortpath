33f9fb4a8f9b39204dbfd874c41bfa538941dea1
Joseph Diza <josephm.diza@gmail.com>
2023-02-07 14:40:32 -0800

    Benchmark serialize_bash

--- benches/shortpaths.rs
+++ benches/shortpaths.rs
@@ -1,34 +1,34 @@
 use shortpaths::{
-    shortpaths::{Shortpath, ShortpathsBuilder, SP, populate_expanded_paths},
+    shortpaths::{Shortpath, SP},
+    builder::{ShortpathsBuilder, ShortpathOperationsExt},
     export::{Export, bash::BashExporter},
 };
 
 use std::path::PathBuf;
 
 use indexmap::indexmap;
-use log::LevelFilter;
-use pretty_env_logger::formatted_timed_builder;
-
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 
 // Benchmarks
-fn bench_populate_dependencies(mut shortpaths: SP) -> SP {
-    populate_expanded_paths(&mut shortpaths)
+
+fn bench_populdate_expanded_paths(shortpaths: &SP) -> SP {
+    shortpaths.populate_expanded_paths()
 }
 
-fn bench_nested_serialize_bash(shortpaths: SP) {
+fn bench_nested_serialize_bash(shortpaths: &SP) {
     let exp = BashExporter::default()
-        .set_shortpaths(&shortpaths);
+        .set_shortpaths(shortpaths);
 
     // Test
     let actual = exp.gen_completions();
     let expect = "#!/bin/bash\n\nexport a=\"aaaa\"\nexport b=\"$a/bbbb\"\nexport c=\"$b/cccc\"\nexport d=\"$c/dddd\"\n";
     assert_eq!(actual, expect, "Bash shell completions are generated in the correct order");
-    assert_eq!(1, 0);
 }
 
 fn criterion_benchmark(c: &mut Criterion) {
     // Enable debug statements
+    //use log::LevelFilter;
+    //use pretty_env_logger::formatted_timed_builder;
     //formatted_timed_builder().filter_level(LevelFilter::Trace).init();
 
     // Initialization
@@ -39,15 +39,15 @@ fn criterion_benchmark(c: &mut Criterion) {
         "a".to_owned() => Shortpath::new(PathBuf::from("aaaa"), None),
     };
 
-    let mut sp_builder = ShortpathsBuilder::new(sp_paths);
-    let shortpaths = sp_builder.build().unwrap();
+    let paths = ShortpathsBuilder::from(sp_paths).build().unwrap();
+    let shortpaths = paths.shortpaths;
 
-    c.bench_function("bench_populate_dependencies sp_paths",
-        |b| b.iter(|| bench_populate_dependencies(black_box(shortpaths.clone()))));
+    // Benchmark
+    c.bench_function("bench_populdate_expanded_paths sp_paths",
+        |b| b.iter(|| bench_populdate_expanded_paths(black_box(&shortpaths))));
 
-    //c.bench_function("bench_nested_serialize_bash", |b| b.iter(|| bench_nested_serialize_bash(shortpaths.clone())));
-
-    //c.bench_function("bench_nested_serialize_bash", |b| b.iter(|| bench_nested_serialize_bash(black_box())));
+    c.bench_function("bench_nested_serialize_bash",
+        |b| b.iter(|| bench_nested_serialize_bash(black_box(&shortpaths))));
 }
 
 criterion_group!(benches, criterion_benchmark);
