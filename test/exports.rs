ec2f1719bffe468b5bbee9577a11bd3670e0aeed
Joseph Diza <josephm.diza@gmail.com>
2023-02-08 14:11:43 -0800

    Update test_serialize_bash

--- test/exports.rs
+++ test/exports.rs
@@ -16,7 +16,7 @@ fn test_serialize_bash() {
 
     // Test
     let actual = exp.gen_completions();
-    let expect = "#!/bin/bash\n\nexport a=\"aaaa\"\nexport b=\"$a/bbbb\"\nexport d=\"$a/dddd\"\nexport c=\"$b/cccc\"\n";
+    let expect = "#!/bin/bash\n\nexport a=\"aaaa\"\nexport b=\"$a/bbbb\"\nexport c=\"$b/cccc\"\nexport d=\"$a/dddd\"\n";
     assert_eq!(actual, expect, "Bash shell completions are generated in the correct order");
 }
 
